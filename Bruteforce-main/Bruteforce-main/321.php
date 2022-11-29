8bb4e6ac63cd8f1590aaf74af856f3f65227b7fc
rexik2107 <63393184+rexik2107@users.noreply.github.com>
2022-11-29 11:32:54 +0300

    Update 321.php

--- Bruteforce-main/Bruteforce-main/321.php
+++ Bruteforce-main/Bruteforce-main/321.php
@@ -1,21 +1,20 @@
 <?php
-// CWE-307: Improper Restriction of Excessive Authentication Attempts
-// Неправильное ограничение чрезмерных попыток аутентификации (НЕТ ЗАЩИТЫ ОТ БРУТФОРСА)
 if( isset( $_GET[ 'Login' ] ) ) {
-	// CWE-20: Improper Input Validation
+	// CWE-116: необходимо экранировать входные данные
 	// Неправильная проверка ввода
 	$user = $_GET[ 'username' ];
 	$pass = $_GET[ 'password' ];
 
 	// CWE-327: Use of a Broken or Risky Cryptographic Algorithm
-	// Использование сломанного или рискованного криптографического алгоритма (MD5)
+	// Использование сломанного или рискованного криптографического алгоритма (MD5), необходимо использовать SHA256
 	$pass = md5( $pass ); 
 
-	// CWE-943: Improper Neutralization of Special Elements in Data Query Logic
-	// Неправильная нейтрализация специальных элементов в логике запроса данных
+	// CWE-116: необходимо экранировать входные данные
 	$query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';"; 
 
 	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+	#CWE-799 Неправильный контроль частоты взаимодействия
+	//необходимо ограничить многократные запросы для предотвращения DOS-атак, например использовать sleep
 	if( $result && mysqli_num_rows( $result ) == 1 ) {
 		// Get users details
 		$row    = mysqli_fetch_assoc( $result );
@@ -23,7 +22,7 @@ if( isset( $_GET[ 'Login' ] ) ) {
 		// Login successful
 
 		// CWE-79: Failure to Preserve Web Page Structure ('Cross-site Scripting')
-		// Неспособность сохранить структуру веб-страницы («Межсайтовый скриптинг») <
+		// Неспособность сохранить структуру веб-страницы («Межсайтовый скриптинг»)
 
 		$html .= "<p>Welcome to the password protected area {$user}</p>";
 		$html .= "<img src=\"{$avatar}\" />";
